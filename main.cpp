73f0278a2457526647f9a8497ff159b6486f548e
CLeonard <cdytlnrd@gmail.com>
2025-09-04 21:39:34 +0000

    completed assignment

--- main.cpp
+++ main.cpp
@@ -2,8 +2,19 @@
 using namespace std;
 
 int main() {
-  const double length = 7.2;
-  const double width = 1.3;
+  const double length = 7.2; //setting length value
+  const double width = 1.3; //setting width value
+  double area = length * width; //calculating area
+  double perimeter = 2 * ( length + width); //calculating perimeter
 
+  cout << "Rectangle Properties: " << endl; //Output title
 
+  //listing outputs
+
+  cout << "Length = " << length << endl; 
+  cout << "Width = " << width << endl;
+  cout << "Area = " << area << endl;
+  cout << "Perimeter = " << perimeter << endl;
+  
+  return 0;
 }
